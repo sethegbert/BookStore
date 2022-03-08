347488c312625a93fc9e56f2f7101c2eba5f084a
sethegbert <90418352+sethegbert@users.noreply.github.com>
2022-03-08 09:36:21 -0700

    added migration to fix errors

--- Migrations/20220308163102_FixTest.Designer.cs
+++ Migrations/20220308163102_FixTest.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using BookStore.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BookStore.Migrations
 {
     [DbContext(typeof(BookStoreContext))]
-    partial class BookStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20220308163102_FixTest")]
+    partial class FixTest
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
