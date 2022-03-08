135f8408f60c9cc50bc04f6f2c95ec96df34bed4
sethegbert <90418352+sethegbert@users.noreply.github.com>
2022-03-07 19:03:14 -0700

    Initial

--- Models/Purchase.cs
+++ Models/Purchase.cs
@@ -35,5 +35,8 @@ namespace BookStore.Models
         public string Country { get; set; }
 
         public bool Anonymous { get; set; }
+
+        [BindNever]
+        public bool Shipped { get; set; }
     }
 }
